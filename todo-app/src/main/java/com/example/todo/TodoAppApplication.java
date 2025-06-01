9b459eaaa092331348f04592a25ed10bb5884a21
user <user@host.docker.internal>
2025-06-01 23:05:15 +0900

    ブラウザ自動起動

--- todo-app/src/main/java/com/example/todo/TodoAppApplication.java
+++ todo-app/src/main/java/com/example/todo/TodoAppApplication.java
@@ -2,12 +2,35 @@ package com.example.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class TodoAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodoAppApplication.class, args);
-	}
+		SpringApplication app = new SpringApplication(TodoAppApplication.class);
 
+		// ✅ 起動完了後にGoogle Chromeで開く
+		app.addListeners((ApplicationListener<ApplicationReadyEvent>) event -> {
+			try {
+				System.out.println("Trying to open browser...");
+
+				// Windows向け：Google Chromeのパス
+				String chromePath = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
+				String url = "http://localhost:8080";
+
+				// ChromeでURLを開く
+				Runtime.getRuntime().exec(new String[] { chromePath, url });
+
+				System.out.println("Chrome should have opened.");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+
+		app.run(args);
+	}
 }
