72a5d656a2751299fa3ccbf64abd5c460904d3a0
user <user@host.docker.internal>
2025-06-19 18:14:57 +0900

    javadoc修正

--- todo-app/src/main/java/com/example/todo/service/TodoService.java
+++ todo-app/src/main/java/com/example/todo/service/TodoService.java
@@ -5,15 +5,36 @@ import com.example.todo.entity.User;
 
 import java.util.List;
 
-/*
- * @param id,todo,user
- * */
+/**
+ * Todo に関するビジネスロジックのインターフェース
+ */
 public interface TodoService {
+    /**
+     * 全てのTodoを取得する
+     */
     List<Todo> findAll();
+
+    /**
+     * IDに対応するTodoを取得する
+     * @param id TodoのID
+     */
     Todo findById(Long id);
+
+    /**
+     * Todoを保存または更新する
+     * @param todo 保存するTodo
+     */
     Todo save(Todo todo);
+
+    /**
+     * 指定されたIDのTodoを削除する
+     * @param id 削除するTodoのID
+     */
     void delete(Long id);
 
-    // ✅ 追加：ユーザーで絞り込む
+    /**
+     * 指定ユーザーのTodo一覧を取得する
+     * @param user ユーザー
+     */
     List<Todo> findByUser(User user);
 }
